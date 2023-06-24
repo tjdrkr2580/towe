8f38160687d3a79b9a8a1975a29a69101105c969
kimtaehyun <tjdrkr2580@naver.com>
2023-06-24 11:50:10 +0900

    style : button icon

--- lib/main.dart
+++ lib/main.dart
@@ -14,46 +14,69 @@ class Towe extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: false,
-          title: const Padding(
-            padding: EdgeInsets.only(left: 10),
-            child: Text(
-              'Towe.',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w800),
-              textAlign: TextAlign.left,
-            ),
+        home: Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: const Padding(
+          padding: EdgeInsets.only(left: 10),
+          child: Text(
+            'Towe.',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w800),
+            textAlign: TextAlign.left,
           ),
-          foregroundColor: Colors.black,
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          actions: [
-            Row(
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.add_box,
-                  ),
-                  splashRadius: 18,
-                  iconSize: 25,
+        ),
+        foregroundColor: Colors.black,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.add_box,
                 ),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.menu),
-                  splashRadius: 18,
-                  iconSize: 25,
+                splashRadius: 18,
+                iconSize: 25,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.menu),
+                splashRadius: 18,
+                iconSize: 25,
+              ),
+            ],
+          ),
+        ],
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(8.0),
+        child: HomeScreen(),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(right: 20, bottom: 50),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 108, 162, 255),
+                  borderRadius: BorderRadius.circular(50)),
+              child: IconButton(
+                iconSize: 30,
+                onPressed: () {},
+                splashColor: Colors.transparent,
+                highlightColor: Colors.transparent,
+                icon: const Icon(
+                  Icons.draw,
+                  color: Colors.white,
                 ),
-              ],
+              ),
             ),
           ],
         ),
-        body: const Padding(
-          padding: EdgeInsets.all(8.0),
-          child: HomeScreen(),
-        ),
       ),
-    );
+    ));
   }
 }
---
+++ lib/widgets/write_button.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WriteButton extends StatelessWidget {
+  const WriteButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton.large(
+      onPressed: () {},
+    );
+  }
+}
