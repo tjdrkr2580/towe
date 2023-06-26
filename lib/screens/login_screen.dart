ce6efe173f6ea84394dda059c6e3baae6098f9f5
kimtaehyun <tjdrkr2580@naver.com>
2023-06-26 11:40:12 +0900

    add : 회원가입, 로그인 버튼

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,13 +9,82 @@ class LoginWidget extends StatefulWidget {
 }
 
 class _LoginWidgetState extends State<LoginWidget> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const ToweAppBar(isMenu: false),
-      body: Container(
-        child: const Text("안녕"),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 28),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const HelloLogin(),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: double.infinity,
+                    height: 55,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromARGB(255, 74, 137, 248)),
+                      child: const Text(
+                        "로그인",
+                        style: TextStyle(
+                            fontSize: 18.5, fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 55,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromARGB(255, 58, 106, 188)),
+                      child: const Text(
+                        "회원가입",
+                        style: TextStyle(
+                            fontSize: 17, fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class HelloLogin extends StatelessWidget {
+  const HelloLogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "어서오세요!",
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+        ),
+        SizedBox(height: 5),
+        Text(
+          "우리 처음 본 사이가 아닌가봐요.",
+          style: TextStyle(fontSize: 17, fontWeight: FontWeight.w600),
+        ),
+      ],
     );
   }
 }
